711153492226af296fa99ebdca55d763d5ccc191
Sagar Yadav <sgrydv0@gmail.com>
2021-11-24 10:10:09 +0530

    Added description of every line of code.

--- rollback_script.sh
+++ rollback_script.sh
@@ -1,9 +1,13 @@
 cd /
+echo "Changing directory to tmp"
 cd home/sagar3yadav/tmp/
+echo "Cleaning tmp directory"
 sudo rm -rf *
+echo "Downlodaing artifacts from Jfrog in /tmp folder"
 sudo wget --user=sagar.yadav@telusinternational.com --password=123@Artifact0ry https://sagar3yadav.jfrog.io/artifactory/deploy/com/web/rock-paper-scissor/$1/rock-paper-scissor-$1.war
+echo "Renaming war file to deploy.war"
 sudo mv rock-paper-scissor-*.war deploy.war
-sudo ls /home/sagar3yadav/web/apache-tomcat-9.0.54/webapps
+echo "Cleaning pervios .war file from tomcat webapss folder"
 sudo rm -rf /home/sagar3yadav/web/apache-tomcat-9.0.54/webapps/deplo*
+echo "Coping artifacts from /tmp folder to tomcat webapps folder"
 sudo cp /home/sagar3yadav/tmp/deploy.war /home/sagar3yadav/web/apache-tomcat-9.0.54/webapps
-pwd
